5d242bbf1aa150bcdde399f0531fbc515c1168cf
Mark <noesen.mark@gmail.com>
2024-03-19 15:17:23 -0700

    Deleted unnecessary files

--- calculate.c
+++
@@ -1,5 +0,0 @@
-#include "calculate.h"
-
-float calculateTotalCost(float tickets, float price) {
-    return tickets * price;
-}
--- calculate.h
+++
@@ -1,6 +0,0 @@
-#ifndef CALCULATE_H
-#define CALCULATE_H
-
-float calculateTotalCost(float tickets, float price);
-
-#endif /* CALCULATE_H */
